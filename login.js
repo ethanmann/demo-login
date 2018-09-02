b264ea268941e817bc0391db3c6dfbe5f51c9061
ethanmann <e.milomann@gmail.com>
2018-09-02 15:19:39 -0500

    added login.js for js

---
+++ login.js
@@ -0,0 +1,26 @@
+window.onload = main;
+
+function main(){
+  // Get the modal id, closeModal class items, and openModal id
+  var modal = document.getElementById('modal');
+  var closeModal = document.getElementsByClassName('closeModal');
+  var openModal = document.getElementById('openModal');
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  }
+
+  // Turn closeModal into an Array
+  var closeModalArray = Array.from(closeModal);
+
+  // When the user clicks on close/cancel, close the modal
+  closeModalArray.forEach(function(element){
+    element.onclick = () => {modal.style.display='none';}
+  });
+
+  // When the user clicks on login, open the modal
+  openModal.onclick = () => {modal.style.display='block'};
+}
