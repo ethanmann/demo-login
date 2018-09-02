a07d94b708ee50543e3990b41f8d4b982f2ec8af
ethanmann <e.milomann@gmail.com>
2018-09-02 15:50:16 -0500

    changed variable names to be unique

--- login.js
+++ login.js
@@ -2,25 +2,25 @@ window.onload = main;
 
 function main(){
   // Get the modal id, closeModal class items, and openModal id
-  var modal = document.getElementById('loginModal');
-  var closeModal = document.getElementsByClassName('closeLoginModal');
-  var openModal = document.getElementById('openLoginModal');
+  var loginModal = document.getElementById('loginModal');
+  var closeLoginModal = document.getElementsByClassName('closeLoginModal');
+  var openLoginModal = document.getElementById('openLoginModal');
 
   // When the user clicks anywhere outside of the modal, close it
   window.onclick = function(event) {
-      if (event.target == modal) {
-          modal.style.display = "none";
+      if (event.target == loginModal) {
+          loginModal.style.display = "none";
       }
   }
 
   // Turn closeModal into an Array
-  var closeModalArray = Array.from(closeModal);
+  var closeLoginModalArray = Array.from(closeLoginModal);
 
   // When the user clicks on close/cancel, close the modal
-  closeModalArray.forEach(function(element){
-    element.onclick = () => {modal.style.display='none';}
+  closeLoginModalArray.forEach(function(element){
+    element.onclick = () => {loginModal.style.display='none';}
   });
 
   // When the user clicks on login, open the modal
-  openModal.onclick = () => {modal.style.display='block';}
+  openLoginModal.onclick = () => {loginModal.style.display='block';}
 }
--- signup.js
+++ signup.js
@@ -2,25 +2,25 @@ window.onload = main;
 
 function main(){
   // Get the modal id, closeModal class items, and openModal id
-  var modal = document.getElementById('signupModal');
-  var closeModal = document.getElementsByClassName('closeSignupModal');
-  var openModal = document.getElementById('openSignupModal');
+  var signupModal = document.getElementById('signupModal');
+  var closeSignupModal = document.getElementsByClassName('closeSignupModal');
+  var openSignupModal = document.getElementById('openSignupModal');
 
   // When the user clicks anywhere outside of the modal, close it
   window.onclick = function(event) {
-      if (event.target == modal) {
-          modal.style.display = "none";
+      if (event.target == signupModal) {
+          signupModal.style.display = "none";
       }
   }
 
   // Turn closeModal into an Array
-  var closeModalArray = Array.from(closeModal);
+  var closeSignupModalArray = Array.from(closeSignupModal);
 
   // When the user clicks on close/cancel, close the modal
-  closeModalArray.forEach(function(element){
-    element.onclick = () => {modal.style.display='none';}
+  closeSignupModalArray.forEach(function(element){
+    element.onclick = () => {signupModal.style.display='none';}
   });
 
   // When the user clicks on login, open the modal
-  openModal.onclick = () => {modal.style.display='block';}
+  openSignupModal.onclick = () => {signupModal.style.display='block';}
 }
