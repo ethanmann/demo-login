05ecd3e40924418a3f13406680a8fc5df927bf17
ethanmann <e.milomann@gmail.com>
2018-09-02 17:56:36 -0500

    added database, querying and conditional checking for server post method, and email checking for HTML form

---
+++ database.py
@@ -0,0 +1,5 @@
+from google.appengine.ext import ndb
+
+class User(ndb.Model):
+    email = ndb.StringProperty(required=True)
+    password = ndb.StringProperty(required=True)
--- main.py
+++ main.py
@@ -2,11 +2,16 @@ import webapp2
 import logging
 import jinja2
 import os
+import database
 
 jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
+
+def plainTextResponse(self, message):
+    self.response.headers['Content-Type'] = 'text/plain'
+    self.response.write(message)
 
 class MainPageHandler(webapp2.RequestHandler):
     def get(self):
@@ -15,24 +20,47 @@ class MainPageHandler(webapp2.RequestHandler):
         self.response.write(template.render())
 
     def post(self):
-        logging.info("POST METHOD")
+        type = str(self.request.get('type'))
 
-        type = self.request.get('type')
-        if type is "login":
-            uname = self.request.get('uname')
+        if type == "login":
+            logging.info("login")
+            uname = self.request.get('email')
             psw = self.request.get('psw')
             remember = self.request.get('remember')
+            #plainTextResponse(self, "LOGIN QUERY: %s, %s, %s" % (uname, psw, remember))
 
-            self.response.headers['Content-Type'] = 'text/plain'
-            self.response.write("LOGIN QUERY: %s, %s, %s" % (uname, psw, remember))
-        elif type is "signup":
+            query = database.User.query(database.User.email == email, database.User.password == psw)
+            results = query.fetch()
+
+            if len(results) == 0:
+                plainTextResponse(self, "The combination of username and password does not exist")
+
+        elif type == "signup":
+            logging.info("signup")
             email = self.request.get('email')
-            psw = self.request.get('psw')
-            psw_repeat = self.request.get('psw-repeat')
-            remember = self.request.get('remember')
 
-            self.response.headers['Content-Type'] = 'text/plain'
-            self.response.write("LOGIN QUERY: %s, %s, %s, %s" % (email, psw, psw_repeat, remember))
+            query = database.User.query(database.User.email == email)
+            results = query.fetch()
+
+            if len(results) > 0:
+                plainTextResponse(self, "This email is already taken.")
+
+            else:
+                psw = self.request.get('psw')
+                psw_repeat = self.request.get('psw-repeat')
+
+                if (psw == psw_repeat):
+                    remember = self.request.get('remember')
+
+                    newUser = User(email=email, password=psw)
+                    newUser.put()
+
+                    plainTextResponse(self, "You have successfully signed up!")
+                    #plainTextResponse(self, "SIGNUP QUERY: %s, %s, %s, %s" % (email, psw, psw_repeat, remember))
+
+                else:
+                    plainTextResponse(self, "The passwords you entered are not the same.")
+
 
 app = webapp2.WSGIApplication([
     ('/', MainPageHandler),
